cb572a2c2f67b6301a02d53d132b220bc998656c
fernando chaim <fmchaim@gmail.com>
2024-06-14 17:02:09 -0300

    controllers e rotas ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/dashboard", to: "users#show", as: :user_profile
 
   resources :patients
   resources :doctors
   resources :appointments
   resources :calendars
+
 end
